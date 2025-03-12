196bbf40abbe9dd8a0d3c0413890f58ede87a7b7
Nikita <nikita.vldm@gmail.com>
2025-03-12 15:11:40 +0300

    bakery tests

--- tasks/model_bakery/baker/main.cpp
+++ tasks/model_bakery/baker/main.cpp
@@ -1,4 +1,20 @@
-int main()
+#include "Baker.hpp"
+#include <filesystem>
+#include <spdlog/spdlog.h>
+
+int main(int argc, char* argv[])
 {
-  return 0;
+    if (argc != 2)
+    {
+        spdlog::error("Model file is required\n");
+        return 1;
+    }
+
+    std::filesystem::path filename = std::filesystem::path(argv[1]);
+    spdlog::info("Baking scene from file: {}", filename.string());
+
+    Baker baker;
+    baker.bakeScene(filename);
+
+    return 0;
 }
